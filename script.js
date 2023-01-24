7d5b64444481dfe8a0429529d70f75f313b0341b
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-01-24 12:56:23 +0600

    push

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 function myFunciton() {
     document.getElementsByClassName("form-control").innerHTML ="jahangir alam";
 }
+
+
+// dark and light info
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
