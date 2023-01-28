106159db9c1fb9122ca92aad39126521ac875556
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-01-29 02:55:46 +0600

    submit

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 })
-
+// Date and time add 
 function doDate() {
   var str = "";
   var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
   var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
   var now = new Date();
 
-  str += "Today is: " + days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+  str += days[now.getDay()] + "<br> " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
     document.getElementById("demo").innerHTML = str;
 }
 setInterval(doDate, 1000);
